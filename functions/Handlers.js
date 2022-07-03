2d85c4155b71696624e73344dd44b31062afcc07
Quintin Willison <q@qwuk.net>
2022-07-03 15:12:37 +0100

    Conform string quoting in error messages.

--- functions/Handlers.js
+++ functions/Handlers.js
@@ -57,28 +57,28 @@ exports.createOrder = async (req, res) => {
 function assertNumber(value) {
   const type = typeof value;
   if (type !== 'number') {
-    throw new RequestError(`Value "${value}", of type "${type}", is not a number.`);
+    throw new RequestError(`Value '${value}', of type '${type}', is not a number.`);
   }
 }
 
 function assertLatitude(latitude) {
   assertNumber(latitude);
   if (latitude < -90.0 || latitude > 90.0) {
-    throw new RequestError(`Latitude "${latitude}" is out of range.`);
+    throw new RequestError(`Latitude '${latitude}' is out of range.`);
   }
 }
 
 function assertLongitude(longitude) {
   assertNumber(longitude);
   if (longitude < -180.0 || longitude > 180.0) {
-    throw new RequestError(`Longitude "${longitude}" is out of range.`);
+    throw new RequestError(`Longitude '${longitude}' is out of range.`);
   }
 }
 
 function assertLocation(location) {
   const type = typeof location;
   if (type !== 'object' || location === null) {
-    throw new RequestError(`Value "${location}", of type "${type}", is not a non-null object.`);
+    throw new RequestError(`Value '${location}', of type '${type}', is not a non-null object.`);
   }
   assertLatitude(location.latitude);
   assertLongitude(location.longitude);
